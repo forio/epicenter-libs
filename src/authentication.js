66f779adfadcba034ad77948deb18008aaf450f6
Wallace Luk <wluk@forio.com>
2019-08-28 16:44:55 -0700

    Add logout

--- src/authentication.js
+++ src/authentication.js
@@ -1,6 +1,16 @@
 import Router from './router.js';
 import store from './store.js';
+import channelManager from './channel-manager.js';
 import * as utility from './utility.js';
+
+export async function logout() {
+    try {
+        await channelManager.disconnect();
+        store.removeItem(utility.AUTH_TOKEN);
+    } catch (err) {
+        throw new utility.EpicenterError('Encountered error while logging out');
+    }
+}
 
 export async function authenticate(options) {
     const { handle, password, group, objectType = 'user', ...others } = options;
@@ -13,6 +23,7 @@ export async function authenticate(options) {
             body: { handle, password, group, objectType },
             includeAuthorization: false,
         });
+    await logout();
 
     store.setItem(utility.AUTH_TOKEN, response.body.session);
     return response;
@@ -27,9 +38,8 @@ export async function upgrade(options) {
         .patch('/authentication', {
             body: { objectType },
         });
+    await logout();
 
     store.setItem(utility.AUTH_TOKEN, response.body.session);
     return response;
 }
-
-
