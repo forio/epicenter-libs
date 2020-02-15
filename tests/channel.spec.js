35654f558ea4c67f90dacf928a94ca8ac589e57b
Wallace Luk <wluk@forio.com>
2020-02-14 16:50:39 -0800

    Add tiny ch test

---
+++ tests/channel.spec.js
@@ -0,0 +1,22 @@
+import chai, { expect } from 'chai';
+chai.use(require('sinon-chai'));
+
+
+describe('Channels', () => {
+    const { Channel, SCOPE_BOUNDARY, PUSH_CATEGORY } = epicenter;
+
+    describe('Path evaluation', () => {
+        /* Added 2020.1.9 -- when we learned cometd's channels were case-sensitive and the server
+         * used lowercase despite the it sending us back values in all caps. */
+        it('Should use lowercase scope boundaries & push categories', async() => {
+            const scopeBoundary = SCOPE_BOUNDARY.GROUP;
+            const pushCategory = PUSH_CATEGORY.SYSTEM;
+            const channel = new Channel({
+                scopeBoundary,
+                scopeKey: 'irrelevant',
+                pushCategory,
+            });
+            expect(channel.path).to.equal(`/${scopeBoundary.toLowerCase()}/irrelevant/${pushCategory.toLowerCase()}`);
+        });
+    });
+});
