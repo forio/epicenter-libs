7b6631a3e46e3e5fccea2ab0d2906e6ecb948628
Wallace Luk <wluk@forio.com>
2020-06-02 18:57:42 -0700

    wip

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,17 +2,6 @@
 export const isBrowser = new Function('try {return this===window;}catch(e){ return false;}');
 export const isNode = new Function('try {return this===global;}catch(e){return false;}');
 
-export const toQueryString = (query) => {
-    if (typeof query === 'string') return query;
-    if (typeof query !== 'object') return '';
-    const queryArray = Array.isArray(query) ? query : Object.entries(query);
-    return queryArray.flatMap(([key, value]) => {
-        if (key === undefined || key === '') return [];
-        if (value === undefined || value === '') return [];
-        if (Array.isArray(value)) return value.map((v) => `${key}=${v}`);
-        return `${key}=${value}`;
-    }).join('&');
-};
 
 export const last = (strOrArr) => strOrArr[strOrArr.length - 1];
 export const prefix = (pre, str) => str.startsWith(pre) ? str : `${pre}${str}`;
