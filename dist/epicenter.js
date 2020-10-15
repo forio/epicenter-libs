99f5fa62128c447a997ca542c4f67c2007751a52
Wallace Luk <wluk@forio.com>
2020-10-15 15:15:05 -0700

    fold .members into .get

--- dist/epicenter.js
+++ dist/epicenter.js
@@ -7418,12 +7418,11 @@ function _remove() {
 /*!*******************************!*\
   !*** ./src/adapters/group.js ***!
   \*******************************/
-/*! exports provided: members, get, update */
+/*! exports provided: get, update */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "members", function() { return members; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "get", function() { return get; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "update", function() { return update; });
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
@@ -7434,24 +7433,28 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-function members(_x) {
-  return _members.apply(this, arguments);
+function get(_x) {
+  return _get.apply(this, arguments);
 }
 
-function _members() {
-  _members = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee(groupKey) {
+function _get() {
+  _get = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee(groupKey) {
     var optionals,
         accountShortName,
         projectShortName,
+        expired,
+        withMembers,
         _args = arguments;
     return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
             optionals = _args.length > 1 && _args[1] !== undefined ? _args[1] : {};
-            accountShortName = optionals.accountShortName, projectShortName = optionals.projectShortName;
+            accountShortName = optionals.accountShortName, projectShortName = optionals.projectShortName, expired = optionals.expired, withMembers = optionals.withMembers;
             _context.next = 4;
-            return new utils__WEBPACK_IMPORTED_MODULE_2__["Router"]().withAccountShortName(accountShortName).withProjectShortName(projectShortName).get("/group/member/".concat(groupKey)).then(function (_ref) {
+            return new utils__WEBPACK_IMPORTED_MODULE_2__["Router"]().withAccountShortName(accountShortName).withProjectShortName(projectShortName).withSearchParams({
+              expired: expired
+            }).get("/group/".concat(withMembers ? 'member/' : '').concat(groupKey)).then(function (_ref) {
               var body = _ref.body;
               return body;
             });
@@ -7466,84 +7469,46 @@ function _members() {
       }
     }, _callee);
   }));
-  return _members.apply(this, arguments);
-}
-
-function get(_x2) {
   return _get.apply(this, arguments);
 }
 
-function _get() {
-  _get = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee2(groupKey) {
+function update(_x2, _x3) {
+  return _update.apply(this, arguments);
+}
+
+function _update() {
+  _update = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee2(groupKey, update) {
     var optionals,
+        runLimit,
         accountShortName,
         projectShortName,
-        expired,
         _args2 = arguments;
     return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            optionals = _args2.length > 1 && _args2[1] !== undefined ? _args2[1] : {};
-            accountShortName = optionals.accountShortName, projectShortName = optionals.projectShortName, expired = optionals.expired;
-            _context2.next = 4;
-            return new utils__WEBPACK_IMPORTED_MODULE_2__["Router"]().withAccountShortName(accountShortName).withProjectShortName(projectShortName).withSearchParams({
-              expired: expired
-            }).get("/group/".concat(groupKey)).then(function (_ref2) {
+            optionals = _args2.length > 2 && _args2[2] !== undefined ? _args2[2] : {};
+            runLimit = update.runLimit;
+            accountShortName = optionals.accountShortName, projectShortName = optionals.projectShortName;
+            _context2.next = 5;
+            return new utils__WEBPACK_IMPORTED_MODULE_2__["Router"]().withAccountShortName(accountShortName).withProjectShortName(projectShortName).patch("/group/".concat(groupKey), {
+              body: {
+                runLimit: runLimit
+              }
+            }).then(function (_ref2) {
               var body = _ref2.body;
               return body;
             });
 
-          case 4:
+          case 5:
             return _context2.abrupt("return", _context2.sent);
 
-          case 5:
+          case 6:
           case "end":
             return _context2.stop();
         }
       }
     }, _callee2);
-  }));
-  return _get.apply(this, arguments);
-}
-
-function update(_x3, _x4) {
-  return _update.apply(this, arguments);
-}
-
-function _update() {
-  _update = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee3(groupKey, update) {
-    var optionals,
-        runLimit,
-        accountShortName,
-        projectShortName,
-        _args3 = arguments;
-    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee3$(_context3) {
-      while (1) {
-        switch (_context3.prev = _context3.next) {
-          case 0:
-            optionals = _args3.length > 2 && _args3[2] !== undefined ? _args3[2] : {};
-            runLimit = update.runLimit;
-            accountShortName = optionals.accountShortName, projectShortName = optionals.projectShortName;
-            _context3.next = 5;
-            return new utils__WEBPACK_IMPORTED_MODULE_2__["Router"]().withAccountShortName(accountShortName).withProjectShortName(projectShortName).patch("/group/".concat(groupKey), {
-              body: {
-                runLimit: runLimit
-              }
-            }).then(function (_ref3) {
-              var body = _ref3.body;
-              return body;
-            });
-
-          case 5:
-            return _context3.abrupt("return", _context3.sent);
-
-          case 6:
-          case "end":
-            return _context3.stop();
-        }
-      }
-    }, _callee3);
   }));
   return _update.apply(this, arguments);
 }
--- src/adapters/group.js
+++ src/adapters/group.js
@@ -1,23 +1,13 @@
 import { Router } from 'utils';
 
-export async function members(groupKey, optionals = {}) {
-    const { accountShortName, projectShortName } = optionals;
-
-    return await new Router()
-        .withAccountShortName(accountShortName)
-        .withProjectShortName(projectShortName)
-        .get(`/group/member/${groupKey}`)
-        .then(({ body }) => body);
-}
-
 export async function get(groupKey, optionals = {}) {
-    const { accountShortName, projectShortName, expired } = optionals;
+    const { accountShortName, projectShortName, expired, withMembers } = optionals;
 
     return await new Router()
         .withAccountShortName(accountShortName)
         .withProjectShortName(projectShortName)
         .withSearchParams({ expired })
-        .get(`/group/${groupKey}`)
+        .get(`/group/${withMembers ? 'member/' : ''}${groupKey}`)
         .then(({ body }) => body);
 }
 
