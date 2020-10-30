4e018e5c8a45e703d2e95573f083331b07fb02dc
Wallace Luk <wluk@forio.com>
2020-10-30 15:16:19 -0700

    build: actually use prod settings

--- webpack.web.prod.js
+++ webpack.web.prod.js
@@ -2,8 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    // mode: 'production',
-    // devtool: 'source-map',
-    mode: 'development',
-    devtool: 'cheap-module-source-map',
+    mode: 'production',
+    devtool: 'source-map',
 });
