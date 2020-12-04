cabad3b0a86938f51ba01244f9d65fdcdf87a2e9
Wallace Luk <wluk@forio.com>
2020-12-04 12:54:08 -0800

    chore: kill bundle analyzer

--- webpack.web.prod.cjs
+++ webpack.web.prod.cjs
@@ -1,11 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.cjs');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
-    plugins: [
-        new BundleAnalyzerPlugin({ analyzerPort: '1234', openAnalyzer: false }),
-    ],
 });
