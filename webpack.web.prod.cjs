2626796474723bd3990448e136d0ae5d6884ce31
Wallace Luk <wluk@forio.com>
2021-03-23 14:39:21 -0700

    test: use development build

--- webpack.web.prod.cjs
+++ webpack.web.prod.cjs
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.cjs');
 
 module.exports = merge(common, {
-    mode: 'production',
+    mode: 'development',
     // devtool: 'source-map',
     devtool: 'cheap-module-source-map',
 });
