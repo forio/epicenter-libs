b073e48eb30119c21fbc08ad769f3a607cc9b5c3
Wallace Luk <wluk@forio.com>
2021-04-26 19:23:45 -0700

    feat: attempt to support formdata (browser)

--- tests/common.js
+++ tests/common.js
@@ -6,6 +6,7 @@ export const ACCOUNT = 'forio-dev';
 export const PROJECT = 'epi-v3';
 
 export const SESSION_KEY = 'com.forio.epicenter.session';
+// TODO -- clean this up before you're able to publish
 export const SESSION = {
     token: 'eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJSUzI1NiJ9.IlVTRVI6MDAwMDAxNmZhMTYzM2FmOTE5N2ZiYzMzYTdiNDA3OTA4MDFjIg.JGyccGD3GPign77s_LvOumHko9TwUfCwMl2FbWEnnNy_ASYaYwBb4mn5X8pnDHS_hsTCeQR6OvPkG5_uOuDbEoBXc93c44Ww_kqqNlKmnv9648ubh4atChk6Fgxy4XjwSWWgef7GB75FGJ0j1PXoGCqADiTNughV5GTaoG-CudxMcNG_zb8lhqSgyV0HI6bB0MKGzCiJpEI_nmK_fuDT6MLRfH_b6cB_dP6s5KDwfIbNAF5o4jw9ly2cHPVnRxYPVzNAqvhqg8NTkq8x5m0zExqdE_8RHvKgWDYW9xyfF5Pj3tCBmxX_-rFnt7KfeOlbBuQssQ0WMa8gnp6GSOoxug',
     expires: true,
