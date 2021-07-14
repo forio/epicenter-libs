ef658d44ce9baebe6feb966c5bc8d945a3aa31c1
Wallace Luk <wluk@forio.com>
2021-07-14 14:23:13 -0700

    build: switch back to production build options

--- webpack.web.prod.cjs
+++ webpack.web.prod.cjs
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.cjs');
 
 module.exports = merge(common, {
-    mode: 'development',
-    // devtool: 'source-map',
-    devtool: 'cheap-module-source-map',
+    mode: 'production',
+    devtool: 'source-map',
+    // devtool: 'cheap-module-source-map',
 });
