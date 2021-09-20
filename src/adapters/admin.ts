679ed3a2cf9ea6b24054087617440fe66890420c
David Berkman <david.berkman@grenadefish.net>
2021-09-19 22:34:12 -0700

    admin

--- src/adapters/admin.ts
+++ src/adapters/admin.ts
@@ -1,5 +1,31 @@
 import {Router} from 'utils/index';
 
+export interface Secret {
+    password: string;
+}
+
+export interface AdminCreateView {
+    handle: string;
+    email: string;
+    givenName?: string;
+    familyName?: string;
+    verified: true;
+    active?: true;
+}
+
+export interface NativeAdminCreateView extends AdminCreateView {
+    objectType: 'native';
+    secret: Secret;
+}
+
+export async function createAdmin(optionals: AdminCreateView) {
+
+    return await new Router()
+        .post('/account', {
+            body: optionals,
+        }).then(({body}) => body);
+}
+
 export async function getWithHandle(handle: string, optionals: GenericAdapterOptions = {}) {
     const {server} = optionals;
     return await new Router()
