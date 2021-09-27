00f887e2b26eb8f809e227c752fe1c92a20f11b0
David Berkman <david.berkman@grenadefish.net>
2021-09-27 00:24:56 -0700

    fix

--- src/adapters/authentication.ts
+++ src/adapters/authentication.ts
@@ -120,7 +120,7 @@ export async function sendValidationEmail(handle, subject) {
     return await new Router()
         .withAccountShortName('epicenter')
         .withProjectShortName('manager')
-        .post(`/validate/${handle}`, {
+        .post(`/authentication'/validate/${handle}`, {
             body: {subject: subject},
         }).then(({body}) => body);
 }
