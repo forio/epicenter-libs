fcf2e68fa41aaf27d80eeb10a0be10588ebd3a01
Jacob Premo <jpremo@forio.com>
2025-09-23 17:41:10 -0400

    feat: added somebody endpoints

--- src/adapters/somebody.ts
+++ src/adapters/somebody.ts
@@ -1,41 +1,26 @@
-import type { GenericSearchOptions } from '../utils/constants';
 import type { RoutingOptions } from '../utils/router';
+import type { GenericScope } from '../utils/constants';
 import { Router } from '../utils';
-/**
- * Adds somebody to an account; somebody is a person who is not a user, but whose information is used in a simulation; requires support level authentication
- *
- * Base URL: POST `https://forio.com/api/v3/{accountShortName}/{projectShortName}/somebody`
- *
- * @example
- * const email = 'test@test.com';
- * const optionals = {
- *      givenName: 'Person',
- *      familyName: 'Family',
- *      acountId: '12345',
- * }
- *
- * epicenter.somebodyAdapter.create(email, optionals);
- *
- * @param {string}  email                           Email of the person being added
 
- * @param {object}  [optionals={}]                  Optional parameters
- * @param {string}  [optionals.givenName]           Given name of new user
- * @param {string}  [optionals.familyName]          Family name of new user
- * @param {string}  [optionals.accountId]           Account Id of new user
- * @returns {somebodyObject}                        Returns a promise that resolves to the newly created somebody
- */
+export interface Somebody {
+    email: string;
+    somebodyKey: string;
+    scope: GenericScope;
+    familyName?: string;
+    givenName?: string;
+}
+
 export async function create(
     email: string,
+    scope: GenericScope,
     optionals: {
         givenName?: string;
         familyName?: string;
-        accountId?: string;
     } & RoutingOptions = {}
-): Promise<Record<string, unknown>> {
+): Promise<Somebody> {
     const {
         givenName,
         familyName,
-        accountId,
         ...routingOptions
     } = optionals;
     return await new Router()
@@ -46,7 +31,7 @@ export async function create(
                     email,
                     givenName,
                     familyName,
-                    accountId,
+                    scope,
                 },
                 ...routingOptions,
             }
@@ -54,46 +39,57 @@ export async function create(
         .then(({ body }) => body);
 }
 
-/**
- * Open search for somebody, returns a page
- * 
- * Base URL: GET `https://forio.com/api/v3/{accountShortName}/{projectShortName}/somebody/search`
- * 
- * @example
- * epicenter.somebodyAdapter.query({
- *      filter: [
- *          'email|=email1@email.com|email2@email.com',         // searches for a specific email(s)
- *          // 'givenName=Person',                              // searches for somebody by given name (first name)
- *          // 'familyName=PersonLastName',                     // searches for somebody by family name (last name)
- *          // 'accountId=0000017dd3bf540e5ada5b1e058f08f20461',  // searches for a specific accountId
- *          // 'accountShortName=acme',                         // specifies the account, typically unnecessary
- *          // 'projectShortName=simulations',                  // specifies the project, typically unnecessary
-
- *      ],
- *      sort: ['+somebody.email'],    // sort all findings by the 'email' field (ascending)
- *      first: 3,                   // page should start with the 4th item found (defaults to 0)
- *      max: 10,                    // page should only include the first 10 items
- *      count: false,               // If set to true this will return the number of items that match the search
- * });
- * @param searchOptions Search options -- for more on Epicenter search options go [here](NOOP link)
- * @param [optionals]   Optional arguments; pass network call options overrides here. Special arguments specific to this method are listed below if they exist.
- * @returns A page for the list of sombodies found
- */
-export async function query(
-    searchOptions: GenericSearchOptions,
+const NOT_FOUND = 404;
+export async function get(
+    somebodyKey: string,
     optionals: RoutingOptions = {}
-): Promise<Record<string, unknown>> {
-    const { filter = [], sort = [], first = 0, max, count = false } = searchOptions;
+): Promise<Somebody> {
+    return await new Router()
+        .get(`/somebody/${somebodyKey}`, optionals)
+        .catch((error) => {
+            if (error.status === NOT_FOUND) return { body: undefined };
+            return Promise.reject(error);
+        }).then(({ body }) => body);
+}
+
+export async function inScope(
+    scope: GenericScope,
+    optionals: {
+        first?: number,
+        max?: number, //max of 300; default of 300
+    } & RoutingOptions = {}
+): Promise<Somebody[]> {
+    const { scopeBoundary, scopeKey } = scope;
+    const {
+        first, 
+        max,
+        ...routingOptions
+    } = optionals;
     const searchParams = {
-        filter: filter.join(';') || undefined,
-        sort: sort.join(';') || undefined,
-        first, max, count,
+        first, max,
     };
     return await new Router()
         .withSearchParams(searchParams)
-        .get('/somebody/search', {
+        .get(`/somebody/in/${scopeBoundary}/${scopeKey}`, {
             paginated: true,
-            ...optionals,
+            ...routingOptions,
         })
-        .then(({ body }) => body);
+        .catch((error) => {
+            if (error.status === NOT_FOUND) return { body: undefined };
+            return Promise.reject(error);
+        }).then(({ body }) => body);
+}
+
+export async function byEmail(
+    email: string,
+    scope: GenericScope,
+    optionals: RoutingOptions = {}
+): Promise<Somebody> {
+    const { scopeBoundary, scopeKey } = scope;
+    return await new Router()
+        .get(`/somebody/with/${scopeBoundary}/${scopeKey}/${email}`, optionals)
+        .catch((error) => {
+            if (error.status === NOT_FOUND) return { body: undefined };
+            return Promise.reject(error);
+        }).then(({ body }) => body);
 }
