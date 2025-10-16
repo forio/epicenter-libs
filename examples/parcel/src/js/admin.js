2bca503e7d0e20a68f16c6ac713cb57328c439e3
sparklerfish <jadams@forio.com>
2025-10-16 13:38:42 -0700

    refactor: standardize behavior of authed examples

--- examples/parcel/src/js/admin.js
+++ examples/parcel/src/js/admin.js
@@ -4,94 +4,99 @@ import { taskAdapter, authAdapter, SCOPE_BOUNDARY, somebodyAdapter } from 'epice
 // import { taskAdapter, authAdapter, SCOPE_BOUNDARY, somebodyAdapter } from 'epicenter';
 
 const session = authAdapter.getLocalSession();
+const unauthedEl = document.getElementById('unauthed');
+const isntAdminEl = document.getElementById('isnt-admin');
 
-if (!session) {
-    window.location = '/login.html';
-}
+const initialize = () => {
+    const scope = {
+        scopeBoundary: SCOPE_BOUNDARY.GROUP,
+        scopeKey: session.groupKey,
+    };
 
-const scope = {
-    scopeBoundary: SCOPE_BOUNDARY.GROUP,
-    scopeKey: session.groupKey,
+    const pseudonymKey = '0000017c1470ec86ea4ddf435aeebc5abfb1';
+
+    document.getElementById('create-task').addEventListener('click', (event) => {
+        event.preventDefault();
+        const taskName = document.getElementById('task-name').value;
+        const defaultName = 'test-task-increment';
+        const payload = {
+            method: 'GET',
+            url: '/increment',
+        };
+        const trigger = {
+            minutes: 1,
+            hours: 0,
+            days: 0,
+            objectType: 'offset',
+        };
+        const cronTrigger = {
+            value: '1 * * * * ?', //triggers every minute
+            objectType: 'cron',
+        };
+        const dateTrigger = {
+            value: '2021-11-29T13:30:30-04:00',
+            objectType: 'date',
+        };
+        const optionals = {
+
+        };
+        taskAdapter.create(scope, taskName ?? defaultName, payload, dateTrigger);
+    });
+
+    document.getElementById('delete-task').addEventListener('click', (event) => {
+        event.preventDefault();
+        const taskId = document.getElementById('taskId').value;
+        taskAdapter.destroy(taskId);
+    });
+
+    document.getElementById('get-task').addEventListener('click', (event) => {
+        event.preventDefault();
+        const taskId = document.getElementById('taskId').value;
+        taskAdapter.get(taskId);
+    });
+
+    document.getElementById('get-task-history').addEventListener('click', (event) => {
+        event.preventDefault();
+        const taskId = document.getElementById('taskId').value;
+        taskAdapter.getHistory(taskId);
+    });
+
+    document.getElementById('get-task-in').addEventListener('click', (event) => {
+        event.preventDefault();
+        taskAdapter.getTaskIn(scope);
+    });
+
+    document.getElementById('get-task-in-2').addEventListener('click', (event) => {
+        event.preventDefault();
+        taskAdapter.getTaskIn(scope, { pseudonymKey });
+    });
+
+    document.getElementById('create-somebody').addEventListener('click', (event) => {
+        event.preventDefault();
+        const email = 'test4@test.com';
+        const optionals = {
+            givenName: 'Test',
+            familyName: 'McTest',
+        };
+        somebodyAdapter.create(email, optionals);
+    });
+
+    document.getElementById('search-somebody').addEventListener('click', (event) => {
+        event.preventDefault();
+        somebodyAdapter.query({
+            filter: [
+                'email|=test4@test.com|test2@test.com',
+            ],
+            sort: ['-somebody.email'],            
+            count: false,               
+        });
+    });
 };
 
-const pseudonymKey = '0000017c1470ec86ea4ddf435aeebc5abfb1';
-
-document.getElementById('create-task').addEventListener('click', (event) => {
-    event.preventDefault();
-    const taskName = document.getElementById('task-name').value;
-    const defaultName = 'test-task-increment';
-    const payload = {
-        method: 'GET',
-        url: '/increment',
-    };
-    const trigger = {
-        minutes: 1,
-        hours: 0,
-        days: 0,
-        objectType: 'offset',
-    };
-    const cronTrigger = {
-        value: '1 * * * * ?', //triggers every minute
-        objectType: 'cron',
-    };
-    const dateTrigger = {
-        value: '2021-11-29T13:30:30-04:00',
-        objectType: 'date',
-    };
-    const optionals = {
-
-    };
-    taskAdapter.create(scope, taskName ?? defaultName, payload, dateTrigger);
-    // taskID: 0000017c1470ec86ea4ddf435aeebc5ac43d
-});
-
-document.getElementById('delete-task').addEventListener('click', (event) => {
-    event.preventDefault();
-    const taskId = document.getElementById('taskId').value;
-    taskAdapter.destroy(taskId);
-});
-
-document.getElementById('get-task').addEventListener('click', (event) => {
-    event.preventDefault();
-    const taskId = document.getElementById('taskId').value;
-    taskAdapter.get(taskId);
-});
-
-document.getElementById('get-task-history').addEventListener('click', (event) => {
-    event.preventDefault();
-    const taskId = document.getElementById('taskId').value;
-    taskAdapter.getHistory(taskId);
-});
-
-document.getElementById('get-task-in').addEventListener('click', (event) => {
-    event.preventDefault();
-    taskAdapter.getTaskIn(scope);
-});
-
-document.getElementById('get-task-in-2').addEventListener('click', (event) => {
-    event.preventDefault();
-    taskAdapter.getTaskIn(scope, { pseudonymKey });
-});
-
-document.getElementById('create-somebody').addEventListener('click', (event) => {
-    event.preventDefault();
-    const email = 'test4@test.com';
-    const optionals = {
-        givenName: 'Test',
-        familyName: 'McTest',
-    };
-    somebodyAdapter.create(email, optionals);
-});
-
-document.getElementById('search-somebody').addEventListener('click', (event) => {
-    event.preventDefault();
-    somebodyAdapter.query({
-        filter: [
-            'email|=test4@test.com|test2@test.com',
-        ],
-        sort: ['-somebody.email'],    
-        // first: 3,                   
-        // max: 10,                    
-        count: false,               
-    });
-});
+if (!session) {
+    unauthedEl.classList.remove('hidden');
+} else if (session.objectType !== 'admin') {
+    isntAdminEl.classList.remove('hidden');
+} else {
+    initialize();
+}
--- examples/parcel/src/js/chat.js
+++ examples/parcel/src/js/chat.js
@@ -20,156 +20,154 @@ const roomQueryInputEl = document.getElementById('room-query');
 const findRoomEl = document.getElementById('find-room');
 const roomFindingsEl = document.getElementById('room-findings');
 
-/* Internal state */
-const scope = {
-    scopeBoundary: SCOPE_BOUNDARY.GROUP,
-    scopeKey: session.groupKey,
-};
-const chatrooms = [
-
-];
-const colors = {};
-let currentChat;
-
-/* Utilities */
-const SIX = 6;
-const toChatroomString = (chat) => {
-    return `${chat.room || 'N/A'} (${chat.chatKey.slice(-SIX)})`;
-};
-const getRandomColor = (senderKey) => {
-    if (!colors[senderKey]) {
-        const RGB_MAX = 165; // should be 256, but I want to exclude brighter values
-        const RED = Math.floor(Math.random() * RGB_MAX);
-        const GREEN = Math.floor(Math.random() * RGB_MAX);
-        const BLUE = Math.floor(Math.random() * RGB_MAX);
-        colors[senderKey] = `rgb(${RED}, ${GREEN}, ${BLUE})`;
-    }
-    return colors[senderKey];
-};
-
-
-/* Rendering (HTML DOM handling) functions */
-const renderNewMessage = ({ id, senderKey, receiverKey, message }) => {
-    const existingEl = document.getElementById(`msg-${id}`);
-    // Do nothing for private messages
-    if (receiverKey && (
-        receiverKey !== session.userKey &&
-        senderKey !== session.userKey
-    )) return undefined;
-    // Do nothing the element exists (trust that it will be properly updated by other parts of the UI)
-    if (existingEl) return existingEl;
-
-    const messageEl = document.createElement('span');
-    messageEl.classList.add('message');
-    if (id === 'system') {
-        messageEl.classList.add('message--system');
-    } else {
-        messageEl.style.setProperty('color', getRandomColor(senderKey));
-        messageEl.classList.add(`message--${senderKey === session.userKey ? 'you' : 'peer'}`);
-    }
-    if (receiverKey) {
-        messageEl.style.setProperty('color', 'limegreen');
-        const suffix = senderKey === session.userKey ?
-            `to ${receiverKey.slice(-SIX)}` :
-            `from ${senderKey.slice(-SIX)}`;
-        messageEl.innerText = `${message} (${suffix})`;
-    } else {
-        messageEl.innerText = message;
-    }
-    if (!id) {
-        messageEl.classList.add('message--ephemoral');
-    } else {
-        messageEl.setAttribute('id', `msg-${id}`);
-    }
-    chatroomEl.append(messageEl);
-    messageEl.scrollIntoView({ behavior: 'smooth' });
-    return messageEl;
-};
-
-const renderCurrentChat = async() => {
-    if (!currentChat) return;
-    chatroomEl.innerHTML = '';
-    const headingMessageEl = document.createElement('span');
-    headingMessageEl.classList.add('message', 'message--system');
-    renderNewMessage({
-        id: 'system',
-        message: `Viewing room: ${toChatroomString(currentChat)}`,
-    });
-    showLoading();
-    const messages = await chatAdapter.getMessages(currentChat.chatKey);
-    hideLoading();
-    messages.reverse().forEach((message) => {
-        renderNewMessage(message);
-    });
-    await new Channel({
-        ...scope,
-        pushCategory: PUSH_CATEGORY.CHAT,
-    }).subscribe(async(data) => {
-        if (data.content.chatMessage.senderKey !== session.userKey) {
-            if (data.type === 'BROADCAST') {
-                renderNewMessage(data.content.chatMessage);
-            } else if (data.type === 'TARGETED') {
-                const privateMessage = (await chatAdapter.getMessages(
-                    currentChat.chatKey,
-                    {
-                        horizon: data.content.chatMessage.id,
-                        maxRecords: 1,
-                    },
-                ))[0];
-                renderNewMessage(privateMessage);
-            }
-        }
-
-    });
-};
-
-const renderListing = (chat) => {
-    const listingEl = document.createElement('li');
-    const anchorEl = document.createElement('a');
-    anchorEl.innerText = toChatroomString(chat);
-    anchorEl.classList.add('link-button');
-    anchorEl.onclick = () => {
-        currentChat = chat;
-        renderCurrentChat();
+const initialize = () => {
+    /* Internal state */
+    const scope = {
+        scopeBoundary: SCOPE_BOUNDARY.GROUP,
+        scopeKey: session.groupKey,
     };
-    listingEl.append(anchorEl);
-    return listingEl;
-};
 
-const renderChatrooms = () => {
-    chatroomListingsEl.innerHTML = '';
-    if (chatrooms.length === 0) {
-        chatroomListingsEl.innerText = 'None found.';
-        return;
-    }
-    chatrooms.forEach((chat) => {
-        const listing = renderListing(chat);
-        chatroomListingsEl.append(listing);
-    });
-};
+    const chatrooms = [
 
-const renderUserOptions = async() => {
-    showLoading();
-    const group = await groupAdapter.get({ augment: 'MEMBERS' });
-    whisperUserEl.innerHTML = '';
-    group.members.forEach(({ user, role }) => {
-        const { userKey, displayName } = user;
-        if (
-            role.toUpperCase() !== 'PARTICIPANT' ||
-            userKey === session.userKey
-        ) {
+    ];
+    const colors = {};
+    let currentChat;
+
+    /* Utilities */
+    const SIX = 6;
+    const toChatroomString = (chat) => {
+        return `${chat.room || 'N/A'} (${chat.chatKey.slice(-SIX)})`;
+    };
+    const getRandomColor = (senderKey) => {
+        if (!colors[senderKey]) {
+            const RGB_MAX = 165; // should be 256, but I want to exclude brighter values
+            const RED = Math.floor(Math.random() * RGB_MAX);
+            const GREEN = Math.floor(Math.random() * RGB_MAX);
+            const BLUE = Math.floor(Math.random() * RGB_MAX);
+            colors[senderKey] = `rgb(${RED}, ${GREEN}, ${BLUE})`;
+        }
+        return colors[senderKey];
+    };
+
+    /* Rendering (HTML DOM handling) functions */
+    const renderNewMessage = ({ id, senderKey, receiverKey, message }) => {
+        const existingEl = document.getElementById(`msg-${id}`);
+        // Do nothing for private messages
+        if (receiverKey && (
+            receiverKey !== session.userKey &&
+            senderKey !== session.userKey
+        )) return undefined;
+        // Do nothing the element exists (trust that it will be properly updated by other parts of the UI)
+        if (existingEl) return existingEl;
+
+        const messageEl = document.createElement('span');
+        messageEl.classList.add('message');
+        if (id === 'system') {
+            messageEl.classList.add('message--system');
+        } else {
+            messageEl.style.setProperty('color', getRandomColor(senderKey));
+            messageEl.classList.add(`message--${senderKey === session.userKey ? 'you' : 'peer'}`);
+        }
+        if (receiverKey) {
+            messageEl.style.setProperty('color', 'limegreen');
+            const suffix = senderKey === session.userKey ?
+                `to ${receiverKey.slice(-SIX)}` :
+                `from ${senderKey.slice(-SIX)}`;
+            messageEl.innerText = `${message} (${suffix})`;
+        } else {
+            messageEl.innerText = message;
+        }
+        if (!id) {
+            messageEl.classList.add('message--ephemoral');
+        } else {
+            messageEl.setAttribute('id', `msg-${id}`);
+        }
+        chatroomEl.append(messageEl);
+        messageEl.scrollIntoView({ behavior: 'smooth' });
+        return messageEl;
+    };
+
+    const renderCurrentChat = async() => {
+        if (!currentChat) return;
+        chatroomEl.innerHTML = '';
+        const headingMessageEl = document.createElement('span');
+        headingMessageEl.classList.add('message', 'message--system');
+        renderNewMessage({
+            id: 'system',
+            message: `Viewing room: ${toChatroomString(currentChat)}`,
+        });
+        showLoading();
+        const messages = await chatAdapter.getMessages(currentChat.chatKey);
+        hideLoading();
+        messages.reverse().forEach((message) => {
+            renderNewMessage(message);
+        });
+        await new Channel({
+            ...scope,
+            pushCategory: PUSH_CATEGORY.CHAT,
+        }).subscribe(async(data) => {
+            if (data.content.chatMessage.senderKey !== session.userKey) {
+                if (data.type === 'BROADCAST') {
+                    renderNewMessage(data.content.chatMessage);
+                } else if (data.type === 'TARGETED') {
+                    const privateMessage = (await chatAdapter.getMessages(
+                        currentChat.chatKey,
+                        {
+                            horizon: data.content.chatMessage.id,
+                            maxRecords: 1,
+                        },
+                    ))[0];
+                    renderNewMessage(privateMessage);
+                }
+            }
+
+        });
+    };
+
+    const renderListing = (chat) => {
+        const listingEl = document.createElement('li');
+        const anchorEl = document.createElement('a');
+        anchorEl.innerText = toChatroomString(chat);
+        anchorEl.classList.add('link-button');
+        anchorEl.onclick = () => {
+            currentChat = chat;
+            renderCurrentChat();
+        };
+        listingEl.append(anchorEl);
+        return listingEl;
+    };
+
+    const renderChatrooms = () => {
+        chatroomListingsEl.innerHTML = '';
+        if (chatrooms.length === 0) {
+            chatroomListingsEl.innerText = 'None found.';
             return;
         }
-        const optionEl = document.createElement('option');
-        optionEl.value = userKey;
-        optionEl.innerText = displayName;
-        whisperUserEl.append(optionEl);
-    });
-    hideLoading();
-};
+        chatrooms.forEach((chat) => {
+            const listing = renderListing(chat);
+            chatroomListingsEl.append(listing);
+        });
+    };
 
-
-const initialize = () => {
+    const renderUserOptions = async() => {
+        showLoading();
+        const group = await groupAdapter.get({ augment: 'MEMBERS' });
+        whisperUserEl.innerHTML = '';
+        group.members.forEach(({ user, role }) => {
+            const { userKey, displayName } = user;
+            if (
+                role.toUpperCase() !== 'PARTICIPANT' ||
+                userKey === session.userKey
+            ) {
+                return;
+            }
+            const optionEl = document.createElement('option');
+            optionEl.value = userKey;
+            optionEl.innerText = displayName;
+            whisperUserEl.append(optionEl);
+        });
+        hideLoading();
+    };
     /* Attach event listeners */
     chatCreateEl.onclick = async() => {
         showLoading();
