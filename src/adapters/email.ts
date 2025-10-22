07e4aafcb6eb6ca81ab1c57cec05682fb42399ce
sparklerfish <jadams@forio.com>
2025-10-22 14:07:59 -0700

    fix: correct emailAdapter jsdocs and parameters

--- src/adapters/email.ts
+++ src/adapters/email.ts
@@ -7,13 +7,17 @@ export enum ENCODING {
 }
 
 /**
- * Sends an email to an individual user; available to participants
+ * Sends an email to an individual user; requires following authentication levels:
+ * - /email/user/{groupKey}/{userKey} - PARTICIPANT
+ * - /email/user/{groupKey}/{userKey}/from/{from} - REVIEWER
+ * - /email/user/{groupKey}/{userKey}/from/{from}/{replyTo} - REVIEWER
+ * - /email/user/{groupKey}/{userKey}/as/{fromUserKey} - REVIEWER
  * @example
- * //Sends an email with a smiley face png attachment
+ * // Sends an email with a smiley face png attachment
  * const groupKey = epicenter.authAdapter.getLocalSession().groupKey;
- * const subject = "check out this drawing!"
- * const body = "I hope you enjoy this smiley face!"
- * const attachements = [{
+ * const subject = 'check out this drawing!';
+ * const emailBody = 'I hope you enjoy this smiley face!';
+ * const attachments = [{
  *      encoding: 'BASE_64',
  *      name: 'testPic',
  *      contentType: 'image/png',
@@ -21,13 +25,13 @@ export enum ENCODING {
  * }];
  *
  * // Sends an email to the address associated with the provided user key, sender will be seen as "System"
- * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, body, { attachments });
+ * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, emailBody, { attachments });
  * // Sends an email to the address associated with the provided user key, sender will be seen as be the address provided by the "from" field
- * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, body, { attachments, from: 'sender@test.com' });
+ * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, emailBody, { attachments, from: 'sender@test.com' });
  * // Sends an email to the address defined at "replyTo", sender will be seen as be the address provided by the "from" field
- * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, body, { attachments, from: 'sender@test.com', replyTo: 'receiver@test.com' });
+ * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, emailBody, { attachments, from: 'sender@test.com', replyTo: 'receiver@test.com' });
  * // Sends an email to the address associated with the provided user key, sender will be seen as the user associated with the "fromUserKey"
- * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, body, { attachments, fromUserKey: '000001796733eef0842f4d6d960997018a33' });
+ * epicenter.emailAdapter.sendEmail(groupKey, '000001796733eef0842f4d6d960997018a3b', subject, emailBody, { attachments, fromUserKey: '000001796733eef0842f4d6d960997018a33' });
  * @param groupKey                              The groupKey in which the email target user exists
  * @param userKey                               The unique userKey for the email target user
  * @param subject                               The subject line for the email.
@@ -35,7 +39,6 @@ export enum ENCODING {
  * @param [optionals]                           Optional parameters
  * @param [optionals.familyNameFirst]           Specifies whether email target's family name will come before their given name. Defaults to false.
  * @param [optionals.html]                      Whether to treat the body as HTML (true) or as plain text (false). Defaults to false.
- * @param [optionals.body]                      The content of the email.
  * @param [optionals.from]                      The email address from which the message will appear to have been sent from. Will be overriden by fromUserKey.
  * @param [optionals.replyTo]                   The email address that will be replyed to by the recipient. Must be used in conjunction with optionals.from.
  * @param [optionals.fromUserKey]               The userKey from which the email will appear to have been sent. The default response address will also be this email.
@@ -54,7 +57,6 @@ export async function sendEmail(
     optionals: {
         familyNameFirst?: string;
         html?: boolean;
-        body?: string;
         from?: string;
         replyTo?: string;
         fromUserKey?: string;
@@ -66,8 +68,18 @@ export async function sendEmail(
         }[];
     } & RoutingOptions = {},
 ): Promise<void> {
-    const { accountShortName, projectShortName, server, familyNameFirst, html, attachments, from, replyTo, fromUserKey } =
-        optionals;
+    const {
+        accountShortName,
+        projectShortName,
+        server,
+        familyNameFirst,
+        html,
+        from,
+        replyTo,
+        fromUserKey,
+        attachments,
+        ...routingOptions
+    } = optionals;
 
     let fromString = '';
     if (fromUserKey) {
@@ -91,6 +103,7 @@ export async function sendEmail(
                 html,
                 attachments,
             },
+            ...routingOptions,
         })
         .then(({ body }) => body);
 }
@@ -99,10 +112,10 @@ export async function sendEmail(
  * Sends an email to an individual admin (someone with epicenter account); Requires support level authentication
  * @param adminKey                              The unique adminKey for the email target
  * @param subject                               The subject line for the email.
- * @param [optionals]                           Optional parameter
+ * @param emailBody                             The body for the email
+ * @param [optionals]                           Optional parameters
  * @param [optionals.familyNameFirst]           Specifies whether email target's family name will come before their given name. Defaults to false.
  * @param [optionals.html]                      Whether to treat the body as HTML (true) or as plain text (false). Defaults to false.
- * @param [optionals.body]                      The content of the email.
  * @param [optionals.attachments]               An array of (binary) objects to include as attachments. All four properties must be included.
  * @param [optionals.attachments[].encoding]    A string specifying the encoding method. See ENCODING for possible values.
  * @param [optionals.attachments[].data]        A string containing the data for the attachment.
@@ -117,7 +130,6 @@ export async function sendEmailToAdmin(
     optionals: {
         familyNameFirst?: string;
         html?: boolean;
-        body?: string;
         attachments?: {
             encoding: keyof typeof ENCODING;
             data: string;
@@ -126,8 +138,15 @@ export async function sendEmailToAdmin(
         };
     } & RoutingOptions = {},
 ): Promise<void> {
-    const { accountShortName, projectShortName, server, familyNameFirst, html, attachments } =
-        optionals;
+    const {
+        accountShortName,
+        projectShortName,
+        server,
+        familyNameFirst,
+        html,
+        attachments,
+        ...routingOptions
+    } = optionals;
 
     return await new Router()
         .withServer(server)
@@ -141,6 +160,7 @@ export async function sendEmailToAdmin(
                 html,
                 attachments,
             },
+            ...routingOptions,
         })
         .then(({ body }) => body);
 }
